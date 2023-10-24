11bf27d8f8bdb94bde4dd5254b858463701378f0
Rishiraj Singh <st22221@ormiston.school.nz>
2023-10-24 14:24:48 +1300

    Completed the look of my code

--- tkinter_code.py
+++ tkinter_code.py
@@ -1,7 +1,6 @@
-import tkinter as tk # Importing text as tk
-from tkinter import ttk # Importing text as ttk
-from tkinter import messagebox # Adding a message box 
-import json # Saving data in json file
+import tkinter as tk
+from tkinter import ttk, messagebox
+import json
 
 # Naming my window as tk
 window = tk.Tk()
@@ -10,13 +9,13 @@ window = tk.Tk()
 window.title("Julie’s Party Hire")
 
 # Geometry of the window
-window.geometry("700x700")
+window.geometry("500x600")
 
-# Defining the Maximun and the minimun size of the window sto its locked and unchanged, (width,hieght)
-window.minsize(700, 700)
-window.maxsize(700, 700)
+# Defining the Maximum and the minimum size of the window to keep it locked and unchanged (width, height)
+window.minsize(500, 600)
+window.maxsize(500, 600)
 
-# giving the window a background colour
+# giving the window a background color
 window.config(bg="#FFF1DC")
 
 # Defining Constants
@@ -26,6 +25,65 @@ Min_num_of_items = 1
 # Define the data structure to store items information
 items_data = {}
 
-# Create a line and draw it on the canvas
-my_rect = tk.Canvas(window, width=800, height=20)
-my_rect.pack()
+#                            <----Labels and Entry Boxes---->
+
+# Function to set the font and position for labels
+
+
+def create_label(text, row, column):
+    label = tk.Label(window, text=text, background="#38B6FF",
+                     font=("Arial", 10))
+    label.grid(row=row, column=column, sticky="e", padx=10, pady=10)
+    return label
+
+# Function to create entry boxes
+
+
+def create_entry(row, column):
+    entry = tk.Entry(window, border=1, relief="solid", fg="#929090")
+    entry.grid(row=row, column=column, sticky="w", padx=10, pady=10)
+    return entry
+
+
+# Label and Entry for Full Name
+name_label = create_label("Full Name:", 0, 0)
+name_entry = create_entry(0, 1)
+
+# Label and Entry for Receipt Number
+receipt_num_label = create_label("Receipt Number:", 1, 0)
+receipt_entry = create_entry(1, 1)
+
+# Label and Entry for Item Hired
+item_label = create_label("Item Hired:", 2, 0)
+item_entry = create_entry(2, 1)
+
+# Label and Entry for Number of Items
+num_item_label = create_label("Number of items:", 3, 0)
+num_item_entry = create_entry(3, 1)
+
+# Submit Button
+submit_button = tk.Button(
+    window, text="Submit", bg="green", fg="black", command=lambda: submit_data())
+submit_button.place(x=320, y=90)
+
+# Update Button
+update_button = tk.Button(
+    window, text="Update", bg="yellow", fg="black", command=lambda: update_data())
+update_button.place(x=320, y=130)
+
+# Function to handle submit button
+
+
+def submit_data():
+    # Add your logic to handle data submission
+    pass
+
+# Function to handle update button
+
+
+def update_data():
+    # Add your logic to handle data update
+    pass
+
+
+window.mainloop()
